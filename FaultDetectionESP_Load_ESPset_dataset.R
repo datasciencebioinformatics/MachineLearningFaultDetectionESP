54f3523e8570fc06e3f9deaea3467d83962414a3
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-05 09:49:04 -0500

    Create FaultDetectionESP_Load_ESPset_dataset.R

---
+++ FaultDetectionESP_Load_ESPset_dataset.R
@@ -0,0 +1,17 @@
+#########################################################################################################
+# The spectrum and features tables were downloaded from https://github.com/NINFA-UFES/ESPset            #
+#########################################################################################################
+# Path to the feature file
+features_file="/home/felipe/googledrive/MachineLearningFaultDetectionESP/ESPset_dataset/features.csv"
+
+# Path to the spectrum file
+spectrum_file="/home/felipe/googledrive/MachineLearningFaultDetectionESP/ESPset_dataset/spectrum.csv"
+#########################################################################################################
+# Load the spectrum file
+spectrum_signals=read.csv(spectrum_file, fill = TRUE, header = FALSE, sep=";")
+
+# Load the features file
+features_signals=read.csv(features_file, fill = TRUE, header = TRUE, sep=";")
+#########################################################################################################
+
+
