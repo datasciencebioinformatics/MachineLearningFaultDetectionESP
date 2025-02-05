32df7cbc5c163b767df9e32f1eefd59f537553e3
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-05 18:14:47 -0500

    Update FaultDetectionESP_Load_ESPset_dataset.R

--- FaultDetectionESP_Load_ESPset_dataset.R
+++ FaultDetectionESP_Load_ESPset_dataset.R
@@ -13,18 +13,35 @@ spectrum_signals=read.csv(spectrum_file, fill = TRUE, header = FALSE, sep=";")
 # Load the features file
 features_signals=read.csv(features_file, fill = TRUE, header = TRUE, sep=";")
 
-# Take the ids as the rownames
-spectrum_signals$id<-rownames(spectrum_signals)
-
 # Re-set the colnames to numbers
 colnames(spectrum_signals)<-1:length(colnames(spectrum_signals))
+
+# Take the ids as the rownames
+spectrum_signals$id<-rownames(spectrum_signals)
 
 # Spectrum and features merged
 spectrum_features_merged<-merge(spectrum_signals,features_signals[,c("id","esp_id","label")],by="id")
 #########################################################################################################
+# Table with the number of signal collected per equipment and per condition
+# table(spectrum_features_merged$esp_id,spectrum_features_merged$label)
+#
+#     Faulty sensor Misalignment Normal Rubbing Unbalance
+#  0              0            0    201       0        51
+#  1             23            0    644      46        34
+#  2             10            0    277      12         1
+#  3             11            0    347      56         6
+#  4             60           52    578       7       103
+#  5             79            0    614      79        58
+#  6              4            0    272       0       102
+#  7              0            0    611       0        72
+#  8            108            0    309       0        69
+#  9              0           18    308      22        73
+#  10             0            0    639      68         7
+#########################################################################################################
 # The spectrum_signals table must be melt. 
 # The id must be kept to identity each signal.
-melt_spectrum_signals<-melt(spectrum_signals,by="id")
+# I stopped here: it is needed to melt the table and keep the three variables (id,esp_id, and label) 
+melt_spectrum_signals<-melt(spectrum_features_merged,by=c("id","esp_id"))
 
 # Each line represents a signal.
 # For each the 6032 vibration signals , there are 12103 collumns. Each collumn represents the amplitude.
