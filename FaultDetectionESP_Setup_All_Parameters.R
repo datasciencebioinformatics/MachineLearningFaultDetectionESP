c73502d6484ce2452d2cbd640ed736b99e2cd164
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-06 07:29:16 -0500

    Create FaultDetectionESP_Setup_All_Parameters.R

---
+++ FaultDetectionESP_Setup_All_Parameters.R
@@ -0,0 +1,5 @@
+# Path to outputfolder
+ouputfolder="/home/felipe/Documents/MachineLearningFaultDetectionESP/ouputfolder/"
+
+# Create the output folder
+a = system(paste("mkdir ",ouputfolder,sep=""), intern = TRUE)
