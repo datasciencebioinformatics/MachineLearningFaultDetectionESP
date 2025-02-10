ba917caf6220062a61946f8d6096a4449c2c5bd6
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-10 07:36:13 -0500

    Update FaultDetectionESP_Load_All_R_Packages.R

--- FaultDetectionESP_Load_All_R_Packages.R
+++ FaultDetectionESP_Load_All_R_Packages.R
@@ -1,3 +1,22 @@
 library("reshape2")
 library("ggplot2")
 library("dplyr")
+
+# Most common time series libraries
+library("dlm")
+library("forecast")
+library("lubridate")
+library("tsa")
+library("tseries")
+library("xts") 
+library("zoo")
+library("expsmooth")
+library("fpp")
+library("TSA")
+library("astsa")
+
+# Hadley Wickham's tidyverse universe
+library("tidyverse")
+
+# To load a library, use the function
+library("xts")
