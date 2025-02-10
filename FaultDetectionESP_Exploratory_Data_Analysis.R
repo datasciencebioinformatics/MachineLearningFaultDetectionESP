5fdbd326223f3e54d7b71b6273fa41576ce54457
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-10 08:20:37 -0500

    Create FaultDetectionESP_Exploratory_Data_Analysis.R

---
+++ FaultDetectionESP_Exploratory_Data_Analysis.R
@@ -0,0 +1,6 @@
+#########################################################################################################
+# The spectrum and features tables were downloaded from https://github.com/NINFA-UFES/ESPset            #
+######################################################################################################
+# 1.1 Exploratory Data Analysis
+# PCA example: analysis of spectral data¶
+
