14bf5d49b55e3d50cf2b6dc3c2e3f7a26ceffff9
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-11 10:03:32 -0500

    Update FaultDetectionESP_Exploratory_Data_Analysis.R

--- FaultDetectionESP_Exploratory_Data_Analysis.R
+++ FaultDetectionESP_Exploratory_Data_Analysis.R
@@ -13,7 +13,10 @@ melt_spectrum_signals<-melt(spectrum_features_merged,id=c("id","esp_id","label")
 
 # Rename collumn
 colnames(melt_spectrum_signals)<-c("id","esp_id","label","frequency_id","amplitude")
-                               
+
+# Add collumn combining esp_id with label
+melt_spectrum_signals$esp_id_label<-paste(melt_spectrum_signals$label,melt_spectrum_signals$esp_id,sep="_")
+
 # Each line represents a signal.
 # For each the 6032 vibration signals , there are 12103 collumns. Each collumn represents the amplitude.
 # Therefore, two collumns are needed, x for the singal and y for the amplitude.
@@ -30,7 +33,7 @@ dev.off()
 spectrum_features_data<-spectrum_features_merged
 
 # Rename collumns of spectrum data
-spectrum_features_data<-spectrum_features_data[,-which(colnames(spectrum_features_data) %in% c("id","esp_id","label"))]
+spectrum_features_data<-spectrum_features_data[,-which(colnames(spectrum_features_data) %in% c("id","esp_id","label","esp_id_label"))]
 
 # center and scale the data before
 # calculation the components
@@ -40,120 +43,13 @@ model.pca <- prcomp(spectrum_features_data,center = FALSE, scale =FALSE, rank. =
 summary(model.pca)
 
 # Plot pca's
-PCA_of_spectral_data_label<-autoplot(model.pca, data = spectrum_features_merged, colour = 'label') + theme_bw() 
-PCA_of_spectral_data_esp_id<-autoplot(model.pca, data = spectrum_features_merged, colour = 'esp_id') + theme_bw()
+PCA_of_spectral_data_label        <-autoplot(model.pca, data = spectrum_features_merged, colour = 'label') + theme_bw() 
+PCA_of_spectral_data_esp_id       <-autoplot(model.pca, data = spectrum_features_merged, colour = 'esp_id') + theme_bw()
+PCA_of_spectral_data_esp_id_label <-autoplot(model.pca, data = spectrum_features_merged, colour = 'esp_id_label') + theme_bw()
 
 # FindClusters_resolution               
 png(filename=paste(output_dir,"Plot_raw_PCA_of_spectral_data.png",sep=""), width = 20, height = 10, res=600, units = "cm")  
   grid.arrange(PCA_of_spectral_data_label, PCA_of_spectral_data_esp_id, ncol = 2, nrow = 1, top = "Raw data") 
-dev.off()
-######################################################################################################
-# 1.1.2  PCA example: analysis of spectral data averaged per equipment
-# The amplitude of each equipment will be averaged per equipment.
-# Expected to find a more clear peaks, as shown in Figure 1 of:
-# https://onlinelibrary.wiley.com/doi/pdf/10.1002/%28SICI%291522-2594%28199903%2941%3A3%3C450%3A%3AAID-MRM4%3E3.0.CO%3B2-9
-# For each equipment, the average of amplitude will be calculated per frequency_id
-# From the spectrum_features_data, take the frequency_id and the esp_id
-
-# Add a collumn with the esp_id combined with the label
-spectrum_features_merged$esp_id_label<-paste(spectrum_features_merged$label,spectrum_features_merged$esp_id,sep="_")
-
-# First the frequency_id in the colnames
-frequency_ids_vector<- sort(colnames(spectrum_features_data))
-
-# Second the esp_id in the colnames
-esp_ids_vector      <- sort(unique(spectrum_features_merged$esp_id))
-
-# Third the esp_id combined with the label
-esp_with_label      <- sort(unique(spectrum_features_merged$esp_id_label))
-
-# A data.frame with frequency and esp
-df_esp_frequency    <- data.frame(matrix(nrow = length(esp_with_label), ncol = length(frequency_ids_vector))) 
-
-# Set the rownames with frtequency names
-colnames(df_esp_frequency)<-frequency_ids_vector
-
-# Set the colnames with frtequency names
-rownames(df_esp_frequency)<-esp_with_label
-
-# For each esp_ids take the average amplitude
-for (esp_with_label_id in esp_with_label)
-{ 
-  # Take all signals from a specific esp
-  signals_from_esp_label<-spectrum_features_merged[spectrum_features_merged$esp_id_label %in% esp_with_label_id,frequency_ids_vector]
-  
-  # For each frequency_id
-  for (frequency_id in frequency_ids_vector)
-  { 
-    # Then, take the average of the amplitude for each frequency_id
-    mean_of_amplitude<-mean(signals_from_esp_label[,frequency_id])
-
-    # Mean of ampplitude for the equipment 
-    df_esp_frequency[esp_with_label_id,frequency_id]<-mean_of_amplitude    
-  }    
-}
-# Set the esp_id
-df_esp_frequency$esp_with_label_id<-rownames(df_esp_frequency)
-
-# Take list with the paired  id
-esp_with_label_id_list<-strsplit(df_esp_frequency$esp_with_label_id,"_",fixed=T)
-
-# Add also the field esp_id
-df_esp_frequency$esp_id<-0
-
-# And the field esp_id
-df_esp_frequency$label<-""
-
-# For each paired id in the list
-for (esp_with_label_id in esp_with_label_id_list)
-{
-  # Add the esp_id and the label
-  df_esp_frequency[paste(esp_with_label_id[1],esp_with_label_id[2],sep="_"),"esp_id"]<-esp_with_label_id[2]
-  df_esp_frequency[paste(esp_with_label_id[1],esp_with_label_id[2],sep="_"),"label"]<-esp_with_label_id[1]
-}
-
-
-# The spectrum_signals table must be melt. 
-# The id must be kept to identity each signal.
-# Melt by multiple ids
-melt_spectrum_signals<-melt(df_esp_frequency,id=c("esp_with_label_id","esp_id","label"))
-
-# Rename collumn
-colnames(melt_spectrum_signals)<-c("esp_with_label_id","esp_id","label","frequency_id","amplitude")
-                               
-# Each line represents a signal.
-# For each the 6032 vibration signals , there are 12103 collumns. Each collumn represents the amplitude.
-# Therefore, two collumns are needed, x for the singal and y for the amplitude.
-
-# Plot the raw data
-ggplot2_raw_data<-ggplot(data = melt_spectrum_signals, aes(x = as.integer(frequency_id), y = amplitude,colour = factor(esp_with_label_id)))+ geom_line(aes(group=esp_with_label_id))+ theme_bw() +   theme(axis.line = element_line(colour = "black"),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(),    panel.background = element_blank())  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))  + ylim(min(melt_spectrum_signals$amplitude), 100) + ggtitle("Average data (per esp)") + xlim(min(as.integer(melt_spectrum_signals$frequency_id)), max(as.integer(melt_spectrum_signals$frequency_id)))
-
-# Plot_raw_vibration_data.png               
-png(filename=paste(output_dir,"Plot_average_vibration_data.png",sep=""), width = 20, height = 20, res=600, units = "cm")  
-  ggplot2_raw_data
-dev.off()
-
-# Subset spectrum data
-spectrum_features_data<-df_esp_frequency
-
-# Rename collumns of spectrum data
-spectrum_features_data<-spectrum_features_data[,-which(colnames(spectrum_features_data) %in% c("id","esp_id","label","esp_with_label_id"))]
-
-# center and scale the data before
-# calculation the components
-model.pca <- prcomp(spectrum_features_data,center = FALSE, scale =FALSE, rank. = 4)
-
-# Display summary of
-summary(model.pca)
-
-# Plot pca's
-PCA_of_spectral_data_label            <-autoplot(model.pca, data = df_esp_frequency, colour = 'label') + theme_bw()  + theme(legend.position="bottom")
-PCA_of_spectral_data_esp_id           <-autoplot(model.pca, data = df_esp_frequency, colour = 'esp_id') + theme_bw() + theme(legend.position="bottom")
-PCA_of_spectral_data_esp_with_label_id<-autoplot(model.pca, data = df_esp_frequency, colour = 'esp_with_label_id') + theme_bw() + theme(legend.position="bottom")
-
-# FindClusters_resolution               
-png(filename=paste(output_dir,"Plot_average_PCA_of_spectral_data.png",sep=""), width = 30, height = 30, res=600, units = "cm")  
-  grid.arrange(PCA_of_spectral_data_label, PCA_of_spectral_data_esp_id, PCA_of_spectral_data_esp_with_label_id,ncol = 3, nrow = 1, top = "Averaged data data") 
 dev.off()
 ######################################################################################################
 # 1.1.4  PCA example: analysis of spectral data after empiric mode decomposition
