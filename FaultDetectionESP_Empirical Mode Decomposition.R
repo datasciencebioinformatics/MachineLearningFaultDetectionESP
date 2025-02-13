be27c70b56b00dcb68e5fbca097e6c90d9825422
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-13 13:04:58 -0500

    Update FaultDetectionESP_Empirical Mode Decomposition.R

--- FaultDetectionESP_Empirical Mode Decomposition.R
+++ FaultDetectionESP_Empirical Mode Decomposition.R
@@ -62,3 +62,44 @@ ggplot2_imf_emd_data<-ggplot(data = melt_df_results_emd, aes(x = as.integer(freq
 png(filename=paste(output_dir,"Plot_imf_emd_data.png",sep=""), width = 20, height = 25, res=600, units = "cm")  
   ggplot2_imf_emd_data
 dev.off()
+
+
+
+#############################################################################################################
+emd_signal_x    <-as.vector(emd(as.numeric(spectrum_features_merged[signal,frequency_id]),as.integer(frequency_id), boundary="none",max.imf=8))
+
+# Set the vectors for numeric, imf, emd and residual
+raw_signal   <-as.numeric(spectrum_features_merged[signal,frequency_id])
+imf_signal   <-emd_signal_x$imf
+emd_signal   <-emd_signal_x$residue
+residue_nimf <-emd_signal_x$nimf
+
+# Compose a data.frame with the variabels
+df_results_emd<-data.frame(Amplitude=raw_signal,imf=imf_signal,emd=emd_signal)
+
+# Set the rownames of the data.frame
+rownames(df_results_emd)<-frequency_id
+
+# Add esp_id, esp_id, label
+df_results_emd$id<-signal
+df_results_emd$frequency_id<-frequency_id
+df_results_emd$esp_id<-features_signals[signal,c("esp_id")]
+df_results_emd$label<-features_signals[signal,c("label")]
+
+#########################################################################################################
+# The spectrum_signals table must be melt.
+# The id must be kept to identity each signal.
+# Melt by multiple ids
+
+# The spectrum_signals table must be melt.
+# The id must be kept to identity each signal.
+# Melt by multiple ids
+melt_df_results_emd<-melt(df_results_emd,id=c("id", "esp_id", "label","frequency_id"))
+
+# Plot the emd  
+ggplot2_imf_emd_data<-ggplot(data = melt_df_results_emd, aes(x = as.integer(frequency_id), y = value))+ facet_grid(vars(variable)) + theme_bw() + geom_line(aes(group=id)) + ggtitle(paste("emd on sigal", signal, "boundary set to none",sep=" "))
+
+# Plot_raw_vibration_data.png              
+png(filename=paste(output_dir,"Plot_imf_emd_all_imfs.png",sep=""), width = 20, height = 25, res=600, units = "cm")  
+  ggplot2_imf_emd_data
+dev.off()
