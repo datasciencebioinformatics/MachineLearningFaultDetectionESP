28c75be88d767d08d5e7fe20463fc53aab083425
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-20 10:57:42 -0300

    Update FaultDetectionESP_FeatureExtraction.R

--- FaultDetectionESP_FeatureExtraction.R
+++ FaultDetectionESP_FeatureExtraction.R
@@ -6,7 +6,7 @@ spectrum_signals=read.csv(spectrum_file, fill = TRUE, header = TRUE, sep=";")
 features_signals=read.csv(features_file, fill = TRUE, header = TRUE, sep=";")
 #########################################################################################################
 # Initiate a data.frame for the results of all signals
-df_root_mean_square_deviation=data.frame(signal=c(),RMS=c())
+df_feature_extraction=data.frame(signal=c(),RMS=c(),peak=c(),peak_to_peak=c())
 
 # Vector to store the frequencies_id
 frequency_id<-colnames(spectrum_features_merged[,-which(colnames(spectrum_features_merged) %in% c("id","esp_id","label","esp_id_label","esp_id_str"))])
@@ -16,11 +16,16 @@ for (signal in rownames(spectrum_features_merged))
 {
   print(signal)
 
-  # Add the results for the signal
-  df_root_mean_square_deviation<-rbind(df_root_mean_square_deviation,data.frame(signal=signal,RMS=rms(as.vector(unlist(spectrum_features_merged[1,frequency_id])), MARGIN = 2)))  
-
   # Add also the peak          (maximum value)
-  # Add also the peak-to-peak (maximum-minimum value)
+  peak<-max(as.vector(unlist(spectrum_features_merged[1,frequency_id])))
   
+  # Add also the peak-to-peak (maximum-minimum value)
+  peak_to_peak<-max(as.vector(unlist(spectrum_features_merged[1,frequency_id])))-min(as.vector(unlist(spectrum_features_merged[1,frequency_id])))
+
+  # Calcula rms
+  rms<-rms(as.vector(unlist(spectrum_features_merged[1,frequency_id])))
+
+  # Add the results for the signal
+  df_root_mean_square_deviation<-rbind(df_feature_extraction,data.frame(signal=signal,RMS=rms,peak=peak,peak_to_peak=peak_to_peak))  
 }
 #########################################################################################################
