452a9142d437dea11de9f07d0ff66a8f31edc274
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-21 08:47:01 -0300

    Update FaultDetectionESP_FeatureExtraction.R

--- FaultDetectionESP_FeatureExtraction.R
+++ FaultDetectionESP_FeatureExtraction.R
@@ -44,12 +44,25 @@ for (signal_id in rownames(spectrum_features_merged))
   b=summary(fit_er)$coefficients[1,2]
 
   # Add the results for the signal
-  df_feature_extraction<-rbind(df_feature_extraction,data.frame(signal=signal_id,RMS=rms,peak=peak,peak_to_peak=peak_to_peak,median=median,a=b,b=b))  
+  df_feature_extraction<-rbind(df_feature_extraction,data.frame(signal=signal_id,RMS=rms,peak=peak,peak_to_peak=peak_to_peak,median=median,a=a,b=b))  
 }
 #########################################################################################################
 write.csv(df_feature_extraction,"/home/felipe/Downloads/df_feature_extraction.csv", row.names = FALSE)
 #########################################################################################################
+# Field withe esp_id as _str
+spectrum_features_merged$esp_id_str<-paste(spectrum_features_merged$esp_id,sep="")
+spectrum_features_merged$label_esp_id<-paste(spectrum_features_merged$label,spectrum_features_merged$esp_id,sep="_")
+
 # Calculate and plot pca
-model.features     <- prcomp(df_feature_extraction[,c("RMS","peak","peak_to_peak","median","RMS","a","b")],center = FALSE, scale =FALSE, na.action = na.omit, rank. = 4)
-PCA_for_features   <-autoplot(model.features, data =spectrum_features_merged, colour = 'label') + theme_bw() + ggtitle("Amplitude")
+model.features     <- prcomp(df_feature_extraction[,c("RMS","peak","peak_to_peak","median","a","b")],center = FALSE, scale =FALSE, na.action = na.omit, rank. = 4)
+
+# Plot pca's
+PCA_of_spectral_data_label          <-autoplot(model.features, data =spectrum_features_merged, colour = 'label') + theme_bw()
+PCA_of_spectral_data_esp_id         <-autoplot(model.features, data =spectrum_features_merged, colour = 'esp_id_str') + theme_bw()
+PCA_of_spectral_data_esp_id_label   <-autoplot(model.features, data =spectrum_features_merged, colour = 'label_esp_id') + theme_bw()
+
+# FindClusters_resolution               
+png(filename=paste(output_dir,"Plot_summary_Features_data.png",sep=""), width = 40, height = 25, res=600, units = "cm")  
+  grid.arrange(PCA_of_spectral_data_label, PCA_of_spectral_data_esp_id,PCA_of_spectral_data_esp_id_label, ncol = 3, nrow = 1, top = "PCA of empirical model decomposition") 
+dev.off()
 #########################################################################################################
