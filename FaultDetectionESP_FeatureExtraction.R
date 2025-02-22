45681e8f36708eae9c7c15046e19e43986ee6910
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-22 13:54:44 -0500

    Update FaultDetectionESP_FeatureExtraction.R

--- FaultDetectionESP_FeatureExtraction.R
+++ FaultDetectionESP_FeatureExtraction.R
@@ -153,13 +153,18 @@ for (signal_id in rownames(spectrum_features_merged[,frequency_id]))
 }
 # center and scale the data before
 # calculation the components
-plot_feature_extraction_peaks<-na.omit(df_feature_extraction_peaks[,c("heax  ","median98_102","rms98_102","peak1x","peak2x","a","b")])
+plot_feature_extraction_peaks<-na.omit(df_feature_extraction_peaks[,c("median8_13","median98_102","rms98_102","peak1x","peak2x","a","b")])
 plot_feature_extraction_peaks<- plot_feature_extraction_peaks[!is.infinite(rowSums(plot_feature_extraction_peaks)),]
 
 model.pca <- prcomp(plot_feature_extraction_peaks,center = FALSE, scale =FALSE, rank. = 4)
 
 # Plot pca's
-PCA_of_spectral_data_label        <-autoplot(model.pca, data = spectrum_features_merged, colour = 'label') + theme_bw() 
+
+
+
+
+
+<-autoplot(model.pca, data = spectrum_features_merged, colour = 'label') + theme_bw() 
 PCA_of_spectral_data_esp_id       <-autoplot(model.pca, data = spectrum_features_merged, colour = 'esp_id_str') + theme_bw()
 PCA_of_spectral_data_esp_id_label <-autoplot(model.pca, data = spectrum_features_merged, colour = 'label_esp_id') + theme_bw()
 
