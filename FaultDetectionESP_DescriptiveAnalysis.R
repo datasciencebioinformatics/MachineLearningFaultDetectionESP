20c0fe5913121e3f893c64d3fc74405401dcb8f5
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-25 08:13:23 -0500

    Update FaultDetectionESP_DescriptiveAnalysis.R

--- FaultDetectionESP_DescriptiveAnalysis.R
+++ FaultDetectionESP_DescriptiveAnalysis.R
@@ -45,9 +45,9 @@ df_skewness  <- matrix(0, ncol = length(SlidingWindows$min), nrow = length(uniqu
 df_kurtosis  <- matrix(0, ncol = length(SlidingWindows$min), nrow = length(unique(spectrum_features_merged$id)))
 #####################################################################################################################################
 # For each signal, the amplitude is taken for all frequency_id
-for (signal_id in rownames(spectrum_features_merged))
+for (signal_id in rownames(spectrum_features_merged)[1:10])
 {
-  print(signal_id)
+  print(as.integer(signal_id))
   
   # Vector to store amplitude for the frequency vector
   amplitude_vector<-spectrum_features_merged[signal_id,frequency_id]
@@ -57,16 +57,25 @@ for (signal_id in rownames(spectrum_features_merged))
   # this for an interval of size w
   SlidingWindows<-descritive.SlidingWindows(as.vector(unlist(amplitude_vector)), w = 50, skewness = "moment", kurtosis = "moment")
 
-  # statistical indicators
-  df_min      <-rbind(df_min,as.vector(SlidingWindows$min))
-  df_max      <-rbind(df_max,as.vector(SlidingWindows$max))
-  df_mean     <-rbind(df_mean,as.vector(SlidingWindows$mean))
-  df_median   <-rbind(df_median,as.vector(SlidingWindows$median))
-  df_sd       <-rbind(df_sd,as.vector(SlidingWindows$sd))
-  df_skewness <-rbind(df_skewness,as.vector(SlidingWindows$skewness))
-  df_kurtosis <-rbind(df_kurtosis,as.vector(SlidingWindows$kurtosis))   
+  # statistical indicators  
+  df_min[as.integer(signal_id),]<-as.vector(SlidingWindows$min)
+  df_max[as.integer(signal_id),]<-as.vector(SlidingWindows$max)
+  df_mean[as.integer(signal_id),]<-as.vector(SlidingWindows$mean)
+  df_median[as.integer(signal_id),]<-as.vector(SlidingWindows$median)
+  df_sd[as.integer(signal_id),]<-as.vector(SlidingWindows$sd)
+  df_skewness[as.integer(signal_id),]<-as.vector(SlidingWindows$skewness)
+  df_kurtosis[as.integer(signal_id),]<-as.vector(SlidingWindows$kurtosis)
+  
 }
-# Preparation of data.frame with the statistical indicators
+df_min<-data.frame(df_min)
+df_max<-data.frame(df_max)
+df_mean<-data.frame(df_mean)
+df_median<-data.frame(df_median)
+df_sd<-data.frame(df_sd)
+df_skewness<-data.frame(df_skewness)
+df_kurtosis<-data.frame(df_kurtosis)
+
+# Preparat1ion of data.frame with the statistical indicators
 # Set rownames
 rownames(df_min)       <-spectrum_features_merged$signal_id[1:length(rownames(df_min))]
 rownames(df_max)       <-spectrum_features_merged$signal_id[1:length(rownames(df_max))]
@@ -76,23 +85,15 @@ rownames(df_sd)        <-spectrum_features_merged$signal_id[1:length(rownames(df
 rownames(df_skewness)  <-spectrum_features_merged$signal_id[1:length(rownames(df_skewness))]
 rownames(df_kurtosis)  <-spectrum_features_merged$signal_id[1:length(rownames(df_kurtosis))]
 
-# Convert all to data.frame
-df_min        <-data.frame(df_min)
-df_max        <-data.frame(df_max)
-df_mean       <-data.frame(df_mean)
-df_median     <-data.frame(df_median)
-df_sd         <-data.frame(df_sd)
-df_skewness   <-data.frame(df_skewness)
-df_kurtosis   <-data.frame(df_kurtosis)
-
 # Set colnames
-colnames(df_min)        <-1:length(SlidingWindows$min)
-colnames(df_max)        <-1:length(SlidingWindows$min)
-colnames(df_mean)       <-1:length(SlidingWindows$min)
-colnames(df_median)     <-1:length(SlidingWindows$min)
-colnames(df_sd)         <-1:length(SlidingWindows$min)
-colnames(df_skewness)   <-1:length(SlidingWindows$min)
-colnames(df_kurtosis)   <-1:length(SlidingWindows$min)
+colnames(df_min)       <- 1:dim(df_min)[2]
+colnames(df_max)       <- 1:dim(df_max)[2]
+colnames(df_mean)      <- 1:dim(df_mean)[2]
+colnames(df_median)    <- 1:dim(df_median)[2]
+colnames(df_sd)        <- 1:dim(df_sd)[2]
+colnames(df_skewness)  <- 1:dim(df_skewness)[2]
+colnames(df_kurtosis)  <- 1:dim(df_min)[2]
+
 
 # Add collumns data.frame 
 df_min          <-cbind(df_min,data.frame(id=rownames(df_min)))
@@ -155,7 +156,7 @@ melt_df_metrics<-rbind(melt_df_min,melt_df_max,melt_df_mean,melt_df_median,melt_
 ggplot2_raw_data<-ggplot(data = melt_df_metrics, aes(x = as.integer(sw_id), y = value))+ geom_line(aes(group=id))+ facet_grid(vars(metric), scales="free") + theme_bw() +   theme(axis.line = element_line(colour = "black"),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(),    panel.background = element_blank())  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + ggtitle("Descritive statistics with sliding windows")
 
 # Plot_raw_vibration_data.png               
-png(filename=paste(output_dir,"Plot_raw_statistical indicators.png",sep=""), width = 20, height = 20, res=600, units = "cm")  
+png(filename=paste(output_dir,"Plot_raw_statistical_indicators.png",sep=""), width = 20, height = 20, res=600, units = "cm")  
   ggplot2_raw_data
 dev.off()
 
@@ -185,3 +186,5 @@ png(filename=paste(output_dir,"Plot_pca_statistical indicators.png",sep=""), wid
   grid.arrange(PCA_of_min,PCA_of_max,PCA_of_median,PCA_of_mean,PCA_of_sd,PCA_of_skewness,PCA_of_kurtosis, ncol = 3, nrow = 3, top = "Descritive statistics with sliding windows")
 dev.off()
 #########################################################################################################
+
+
