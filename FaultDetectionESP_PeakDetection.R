65a4a08c818626dd9e42b69b5e5ee51ca74332bf
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-02-26 13:55:33 -0500

    Update FaultDetectionESP_PeakDetection.R

--- FaultDetectionESP_PeakDetection.R
+++ FaultDetectionESP_PeakDetection.R
@@ -37,7 +37,7 @@ for (signal_id in rownames(spectrum_features_merged))
   # Calculate the peaks
   #peaks<-findpeaks(amplitude_vector,npeaks=3, sortstr=FALSE,minpeakheight=0.02)
 
-  indexes<-data.frame(t(which.minn(amplitude_vector, 3)))
+  indexes<-data.frame(t(which.maxn(amplitude_vector, 3)))
   amplitude<-data.frame(t(amplitude_vector[as.vector(unlist(indexes))]))
   
 
