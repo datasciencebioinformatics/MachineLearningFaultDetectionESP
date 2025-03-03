66bea31dbba464f827c6c7087649d0de27bad3c3
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-03 10:04:06 -0300

    Update FaultDetectionESP_TrainModel.R

--- FaultDetectionESP_TrainModel.R
+++ FaultDetectionESP_TrainModel.R
@@ -1,34 +1,27 @@
 #########################################################################################################
-# Support machine models (SVM)
-https://rpubs.com/uky994/593668
-
-# K-Nearest Neighbor (KNN)
-https://rpubs.com/njvijay/16444
-
-# Naive Bayes (NB)
-https://rpubs.com/maulikpatel/224581
-
-# Stacked AutoEncoder Deep Neural Network (method = 'dnn').
-# Stacked AutoEncoder Deep Neural Network
-https://topepo.github.io/caret/train-models-by-tag.html#support-vector-machines
-
-# "Convolutional Neural Network" R caret
-https://rpubs.com/rusdipermana/image-cl
-
-# Multi-Layer Perceptron
-# Stacked AutoEncoder Deep Neural Network
-https://topepo.github.io/caret/train-models-by-tag.html#neural-network
-#########################################################################################################
 # Split into trainning and testing
 trainning<- as.vector(createDataPartition(features_signals$label,times = 1,p = 0.5,list = TRUE)[[1]])
 testing <- which(!rownames(features_signals) %in% trainning)
 
 # Store trainning and testing data
-trainning_featurea<-features_signals[trainning,c("label","median.8.13.","rms.98.102.","median.98.102.","peak1x","peak2x","a","b")]
+trainning_features<-features_signals[trainning,c("label","median.8.13.","rms.98.102.","median.98.102.","peak1x","peak2x","a","b")]
 testing_featurea  <-features_signals[trainning,c("label","median.8.13.","rms.98.102.","median.98.102.","peak1x","peak2x","a","b")]
 #########################################################################################################
 # Basic Parameter Tuning
 fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
 
 # Traing svm dataset
-svm_espset <- train(label ~ ., data = trainning_featurea, method = "svm", trControl = fitControl, verbose = FALSE)
+svm_espset <- train(label ~ ., data = trainning_features, method = "svmRadial", trControl = fitControl, verbose = FALSE, metric = "ROC")
+knn_espset <- train(label ~ ., data = trainning_features, method = "knn", trControl = fitControl, verbose = FALSE, metric = "ROC")
+nb_espset <- train(label ~ ., data = trainning_features, method = "nb", trControl = fitControl, verbose = FALSE, metric = "ROC")
+dnn_espset <- train(label ~ ., data = trainning_features, method = "dnn", trControl = fitControl, verbose = FALSE, metric = "ROC")
+mlp_espset <- train(label ~ ., data = trainning_features, method = "dnn", trControl = fitControl, verbose = FALSE, metric = "ROC")
+#########################################################################################################
+
+
+
+
+
+
+# To do:
+- Take models directly from vibration dataset
