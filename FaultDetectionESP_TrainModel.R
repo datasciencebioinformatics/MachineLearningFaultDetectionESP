cbaa5d971428221efc9041e0c82a3d8b52f121b9
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-03 15:45:12 -0300

    Update FaultDetectionESP_TrainModel.R

--- FaultDetectionESP_TrainModel.R
+++ FaultDetectionESP_TrainModel.R
@@ -32,7 +32,7 @@ testing <- which(!rownames(features_signals) %in% trainning)
 
 # Store trainning and testing data
 trainning_features<-features_signals[trainning,c("Class","median.8.13.","rms.98.102.","median.98.102.","peak1x","peak2x","a","b")]
-testing_featurea  <-features_signals[trainning,c("Class","median.8.13.","rms.98.102.","median.98.102.","peak1x","peak2x","a","b")]
+testing_features  <-features_signals[trainning,c("Class","median.8.13.","rms.98.102.","median.98.102.","peak1x","peak2x","a","b")]
 #########################################################################################################
 # Basic Parameter Tuning
 fitControl <- trainControl(method = "repeatedcv",
@@ -72,4 +72,4 @@ png(filename=paste(output_dir,"Plot_bwplot_results.png",sep=""), width = 25, hei
   bwplot(resamps, layout = c(3, 1))
 dev.off()
 #########################################################################################################
-bh_pred <- predict(svm_1_espset, bh_te) 
+svm_pred <- predict(svm_1_espset, testing_features) 
