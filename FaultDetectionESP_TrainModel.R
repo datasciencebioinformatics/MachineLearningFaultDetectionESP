51cca8f5cc025e9e4d237bd4033178dc7f7e0d77
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-03 17:50:54 -0300

    Update FaultDetectionESP_TrainModel.R

--- FaultDetectionESP_TrainModel.R
+++ FaultDetectionESP_TrainModel.R
@@ -86,19 +86,16 @@ varImp_mlp_espset   <- varImp(dnn_espset, scale = FALSE)
 varImp_glm_espset   <- varImp(glm_espset, scale = FALSE)
 varImp_dnn_espset   <- varImp(dnn_espset, scale = FALSE)
 
-plot(varImp_svm_1_espset, main = "svmLinear") 
-plot(varImp_svm_2_espset, main = "svmRadial") 
-plot(varImp_mlp_espset, main = "mlp") 
-plot(varImp_knn_espset, main = "knn") 
-plot(varImp_glm_espset, main = "glm") 
-plot(varImp_dnn_espset, main = "dnn") 
+plot_varImp_svm_1_espset<-plot(varImp_svm_1_espset, main = "svmLinear") 
+plot_varImp_svm_2_espset<-plot(varImp_svm_2_espset, main = "svmRadial") 
+plot_varImp_mlp_espset<-plot(varImp_mlp_espset, main = "mlp") 
+plot_varImp_knn_espset<-plot(varImp_knn_espset, main = "knn") 
+plot_varImp_glm_espset<-plot(varImp_glm_espset, main = "glm") 
+plot_varImp_dnn_espset<-plot(varImp_dnn_espset, main = "dnn") 
+
+# bwplo               
+png(filename=paste(output_dir,"Variable_Importance_results.png",sep=""), width = 25, height = 25, res=600, units = "cm")  
+  grid.arrange(plot_varImp_svm_1_espset,plot_varImp_svm_2_espset,plot_varImp_mlp_espset,plot_varImp_knn_espset,plot_varImp_glm_espset,plot_varImp_dnn_espset)
+dev.off()
+
 #########################################################################################################
-cm_svm_1  <-confusionMatrix(data = svm_1_pred, reference = testing_features$Class)
-cm_svm_2  <-confusionMatrix(data = svm_2_pred, reference = testing_features$Class)
-cm_knn    <-confusionMatrix(data = knn_pred, reference = testing_features$Class)
-cm_mlp    <-confusionMatrix(data = mlp_pred, reference = testing_features$Class)
-dnn_mlp   <-confusionMatrix(data = dnn_pred, reference = testing_features$Class)
-glm_mlp   <-confusionMatrix(data = glm_pred, reference = testing_features$Class)
-
-
-plot(svm_1_espset, top = 10)
