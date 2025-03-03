4b5926fbe3bff6329fc9b86d6832ff13e5f9a580
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-03 12:21:21 -0300

    Update FaultDetectionESP_TrainModel.R

--- FaultDetectionESP_TrainModel.R
+++ FaultDetectionESP_TrainModel.R
@@ -33,19 +33,20 @@ testing_featurea  <-features_signals[trainning,c("Class","median.8.13.","rms.98.
 # Basic Parameter Tuning
 fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 3, classProbs = TRUE)
 
-svm_1_espset <- train(Class ~ ., data = trainning_features, method = "svmLinear", trControl = fitControl,metric=TRUE)
-svm_2_espset <- train(Class ~ ., data = trainning_features, method = "svmRadial", trControl = fitControl,metric=TRUE)
-knn_espset <-   train(Class ~ ., data = trainning_features, method = "knn", trControl = fitControl,metric=TRUE)
-mlp_espset    <-   train(Class ~ ., data = trainning_features, method = "mlp", trControl = fitControl,metric=TRUE)
-dnn_espset    <-   train(Class ~ ., data = trainning_features, method = "dnn", trControl = fitControl,metric=TRUE)
-
+svm_1_espset  <- train(Class ~ ., data = trainning_features, method = "svmLinear", trControl = fitControl)
+svm_2_espset  <- train(Class ~ ., data = trainning_features, method = "svmRadial", trControl = fitControl)
+knn_espset    <- train(Class ~ ., data = trainning_features, method = "knn", trControl = fitControl)
+mlp_espset    <- train(Class ~ ., data = trainning_features, method = "mlp", trControl = fitControl
+dnn_espset    <- train(Class ~ ., data = trainning_features, method = "dnn", trControl = fitControl)
 
 #########################################################################################################
 resamps <- resamples(list(svmLinear = svm_1_espset, 
                           svmRadial = svm_2_espset,
-                          knn = knn_espset))
-
-
+                          knn = knn_espset,
+                          mlp=mlp_espset,
+                          dnn=dnn_espset))                          
+                         
+#########################################################################################################
 theme1 <- trellis.par.get()
 theme1$plot.symbol$col = rgb(.2, .2, .2, .4)
 theme1$plot.symbol$pch = 16
