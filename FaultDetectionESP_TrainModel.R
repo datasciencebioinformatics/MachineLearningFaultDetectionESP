0f6d867ea4e20912c0015c2a7d8b14ed38f0abc2
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-03 10:58:28 -0300

    Update FaultDetectionESP_TrainModel.R

--- FaultDetectionESP_TrainModel.R
+++ FaultDetectionESP_TrainModel.R
@@ -11,13 +11,12 @@ testing_featurea  <-features_signals[trainning,c("label","median.8.13.","rms.98.
 fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
 
 # Traing svm dataset
-svm_espset <- train(label ~ ., data = trainning_features, method = "svmRadial", trControl = fitControl, verbose = FALSE, metric = "ROC")
-knn_espset <- train(label ~ ., data = trainning_features, method = "knn", trControl = fitControl, verbose = FALSE, metric = "ROC")
-nb_espset <- train(label ~ ., data = trainning_features, method = "nb", trControl = fitControl, verbose = FALSE, metric = "ROC")
-dnn_espset <- train(label ~ ., data = trainning_features, method = "dnn", trControl = fitControl, verbose = FALSE, metric = "ROC")
-mlp_espset <- train(label ~ ., data = trainning_features, method = "dnn", trControl = fitControl, verbose = FALSE, metric = "ROC")
+svm_espset <- train(label ~ ., data = trainning_features, method = "svmRadial", trControl = fitControl, verbose = FALSE)
+knn_espset <- train(label ~ ., data = trainning_features, method = "knn", trControl = fitControl, verbose = FALSE)
+nb_espset <- train(label ~ ., data = trainning_features, method = "nb", trControl = fitControl, verbose = FALSE.)
+dnn_espset <- train(label ~ ., data = trainning_features, method = "dnn", trControl = fitControl, verbose = FALSE)
+mlp_espset <- train(label ~ ., data = trainning_features, method = "dnn", trControl = fitControl, verbose = FALSE)
 #########################################################################################################
-
 
 
 
