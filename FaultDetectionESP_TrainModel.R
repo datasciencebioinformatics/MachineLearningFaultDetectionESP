77a958c71e31506f6602849c75034c23c5481772
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-03 08:11:01 -0300

    Create FaultDetectionESP_TrainModel.R

---
+++ FaultDetectionESP_TrainModel.R
@@ -0,0 +1,9 @@
+#########################################################################################################
+# The spectrum and features tables were downloaded from https://github.com/NINFA-UFES/ESPset            #
+#########################################################################################################
+# Path to the feature file
+features_file="/home/felipe/googledrive/MachineLearningFaultDetectionESP/ESPset_dataset/features.csv"
+
+# Path to the spectrum file
+spectrum_file="/home/felipe/googledrive/MachineLearningFaultDetectionESP/ESPset_dataset/spectrum.csv"
+#########################################################################################################
