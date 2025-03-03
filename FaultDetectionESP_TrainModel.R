87f85687c1c44436f76374555d648f8b802b5dcd
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-03 08:20:44 -0300

    Update FaultDetectionESP_TrainModel.R

--- FaultDetectionESP_TrainModel.R
+++ FaultDetectionESP_TrainModel.R
@@ -7,3 +7,8 @@ features_file="/home/felipe/googledrive/MachineLearningFaultDetectionESP/ESPset_
 # Path to the spectrum file
 spectrum_file="/home/felipe/googledrive/MachineLearningFaultDetectionESP/ESPset_dataset/spectrum.csv"
 #########################################################################################################
+# Support machine models (SVM)
+https://rpubs.com/uky994/593668
+
+# K-Nearest Neighbor (KNN)
+https://rpubs.com/njvijay/16444
