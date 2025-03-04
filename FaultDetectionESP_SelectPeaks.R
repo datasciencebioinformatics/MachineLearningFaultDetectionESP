f6b0215b7f8198bc40af6cf333052473bf0cb779
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-04 10:39:05 -0300

    Update FaultDetectionESP_SelectPeaks.R

--- FaultDetectionESP_SelectPeaks.R
+++ FaultDetectionESP_SelectPeaks.R
@@ -103,6 +103,25 @@ mlp_espset    <- train(Class ~ ., data = trainning_amplitude_in_peaks, method = 
 dnn_espset    <- train(Class ~ ., data = trainning_amplitude_in_peaks, method = "dnn", trControl = fitControl,metric="ROC",na.action=na.omit)
 glm_espset    <- train(Class ~ ., data = trainning_amplitude_in_peaks, method = "glm", trControl = fitControl,metric="ROC",na.action=na.omit)
 #########################################################################################################
+varImp_svm_1_espset <- varImp(svm_1_espset, scale = FALSE)
+varImp_svm_2_espset <- varImp(svm_2_espset, scale = FALSE)
+varImp_knn_espset   <- varImp(knn_espset, scale = FALSE)
+varImp_mlp_espset   <- varImp(dnn_espset, scale = FALSE)
+varImp_glm_espset   <- varImp(glm_espset, scale = FALSE)
+varImp_dnn_espset   <- varImp(dnn_espset, scale = FALSE)
+
+plot_varImp_svm_1_espset<-plot(varImp_svm_1_espset, main = "svmLinear") 
+plot_varImp_svm_2_espset<-plot(varImp_svm_2_espset, main = "svmRadial") 
+plot_varImp_mlp_espset<-plot(varImp_mlp_espset, main = "mlp") 
+plot_varImp_knn_espset<-plot(varImp_knn_espset, main = "knn") 
+plot_varImp_glm_espset<-plot(varImp_glm_espset, main = "glm") 
+plot_varImp_dnn_espset<-plot(varImp_dnn_espset, main = "dnn") 
+
+# bwplo               
+png(filename=paste(output_dir,"Variable_Importance_results_peaks.png",sep=""), width = 25, height = 25, res=600, units = "cm")  
+  grid.arrange(plot_varImp_svm_1_espset,plot_varImp_svm_2_espset,plot_varImp_mlp_espset,plot_varImp_knn_espset,plot_varImp_glm_espset,plot_varImp_dnn_espset)
+dev.off()
+#########################################################################################################
 resamps <- resamples(list(svmLinear = svm_1_espset, 
                           svmRadial = svm_2_espset,
                           knn = knn_espset,
@@ -119,33 +138,7 @@ theme1$plot.line$lwd <- 2
 trellis.par.set(theme1)
 
 # bwplo               
-png(filename=paste(output_dir,"Plot_bwplot_results.png",sep=""), width = 25, height = 12, res=600, units = "cm")  
+png(filename=paste(output_dir,"Plot_bwplot_results_peaks.png",sep=""), width = 25, height = 12, res=600, units = "cm")  
   bwplot(resamps, layout = c(3, 1))
 dev.off()
 #########################################################################################################
-svm_1_pred <- predict(svm_1_espset, testing_features) 
-svm_2_pred <- predict(svm_2_espset, testing_features) 
-knn_pred <- predict(knn_espset, testing_features) 
-mlp_pred <- predict(mlp_espset, testing_features) 
-dnn_pred <- predict(dnn_espset, testing_features) 
-glm_pred <- predict(glm_espset, testing_features) 
-#########################################################################################################
-varImp_svm_1_espset <- varImp(svm_1_espset, scale = FALSE)
-varImp_svm_2_espset <- varImp(svm_2_espset, scale = FALSE)
-varImp_knn_espset   <- varImp(knn_espset, scale = FALSE)
-varImp_mlp_espset   <- varImp(dnn_espset, scale = FALSE)
-varImp_glm_espset   <- varImp(glm_espset, scale = FALSE)
-varImp_dnn_espset   <- varImp(dnn_espset, scale = FALSE)
-
-plot_varImp_svm_1_espset<-plot(varImp_svm_1_espset, main = "svmLinear") 
-plot_varImp_svm_2_espset<-plot(varImp_svm_2_espset, main = "svmRadial") 
-plot_varImp_mlp_espset<-plot(varImp_mlp_espset, main = "mlp") 
-plot_varImp_knn_espset<-plot(varImp_knn_espset, main = "knn") 
-plot_varImp_glm_espset<-plot(varImp_glm_espset, main = "glm") 
-plot_varImp_dnn_espset<-plot(varImp_dnn_espset, main = "dnn") 
-
-# bwplo               
-png(filename=paste(output_dir,"Variable_Importance_results.png",sep=""), width = 25, height = 25, res=600, units = "cm")  
-  grid.arrange(plot_varImp_svm_1_espset,plot_varImp_svm_2_espset,plot_varImp_mlp_espset,plot_varImp_knn_espset,plot_varImp_glm_espset,plot_varImp_dnn_espset)
-dev.off()
-
