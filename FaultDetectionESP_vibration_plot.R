69abe6572a62124a7e31bdb462cfc09a6f94b131
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-11 10:44:04 -0300

    Update FaultDetectionESP_vibration_plot.R

--- FaultDetectionESP_vibration_plot.R
+++ FaultDetectionESP_vibration_plot.R
@@ -105,7 +105,7 @@ melt_spectrum_signals$frequency_id<-as.numeric(as.vector(melt_spectrum_signals$f
 ggplot2_raw_data<-ggplot(data = melt_spectrum_signals, aes(x = frequency_id, y = amplitude,colour = factor(label)))+ geom_line(aes(group=label))+ facet_grid(vars(label),scales="free") + theme_bw() +   theme(axis.line = element_line(colour = "black"),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(),    panel.background = element_blank())   + ggtitle("Averaged data")   +  ylim(0, 0.1) + xlab("spectrum")+ ylab("inches/s")
 
 # Plot_raw_vibration_data.png               
-png(filename=paste(output_dir,"Plot_raw_vibration_convert.png",sep=""), width = 20, height = 20, res=600, units = "cm")  
+png(filename=paste(output_dir,"Plot_raw_vibration_average.png",sep=""), width = 20, height = 20, res=600, units = "cm")  
   ggplot2_raw_data
 dev.off()
 #########################################################################################################
