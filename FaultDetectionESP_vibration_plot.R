0b3a46d79a853b0f3f6f275390ac284c19895c07
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-13 10:08:18 -0300

    Update FaultDetectionESP_vibration_plot.R

--- FaultDetectionESP_vibration_plot.R
+++ FaultDetectionESP_vibration_plot.R
@@ -181,7 +181,7 @@ melt_spectrum_signals_filtered<-melt_spectrum_signals[(melt_spectrum_signals$esp
 
 # Plot_raw_vibration_data.png               
 png(filename=paste(output_dir,"Plot_raw_vibration_signal_filtered.png",sep=""), width = 20, height = 20, res=600, units = "cm")  
-ggplot(data = melt_spectrum_signals_filtered, aes(x = frequency_id, y = amplitude,colour = factor(label)))+ geom_line()+ facet_grid(vars(id),scales="free") + theme_bw()  + ylim(0,0.03) + scale_x_continuous(name="X rotation", limits=c(0, max(melt_spectrum_signals$frequency_id)),breaks=seq(0,max(melt_spectrum_signals$frequency_id),by=0.25))  + xlim(0,2.25)
+ggplot(data = melt_spectrum_signals_filtered, aes(x = frequency_id, y = amplitude,colour = factor(label)))+ geom_line()+ facet_grid(vars(id),scales="free") + theme_bw()  + ylim(0,0.03) + scale_x_continuous(name="X rotation", limits=c(0, max(melt_spectrum_signals$frequency_id)),breaks=seq(0,max(melt_spectrum_signals$frequency_id),by=0.25))  + xlim(0,2.25) + xlab("X rotation")
 dev.off()
 
 
